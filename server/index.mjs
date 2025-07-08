4a7dba7638e8f689116f1473cbd2ce80350ff29a
Richard Gao <richard.g2002@gmail.com>
2025-07-08 16:44:16 -0500

    Create V1 API
    Includes post, patch, delete, get for sessions and users. Get has
    querying capabilities

--- server/index.mjs
+++ server/index.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
-import posts from "./routes/posts.mjs";
+import api_v1 from "./routes/api_V1.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Load the /posts routes
-app.use("/posts", posts);
+app.use("/api/v1", api_v1);
 
 // Global error handling
 app.use((err, _req, res, next) => {
